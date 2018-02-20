eba1504d48e7796f6d0aa600b345a3f9f2a692cd
Gal Ron <galron2000@gmail.com>
2018-02-20 15:14:26 +0200

    Results for graph drawing with hamming distance metric

--- main_kmer_graph_drawing.py
+++ main_kmer_graph_drawing.py
@@ -12,6 +12,7 @@ class Kmer:
 
 # retrieve list of frequent kmers
 kmers = list()
+gene_count_dict = {} # gene_count[KMER] = NUMBER_OF_PROTEINS_WITH_THAT_KMER
 with open(params.FREQUENT_KMERS, 'rb') as kmercsv:
     csvreader = csv.reader(kmercsv, delimiter=',')
     next(csvreader, None) # skip header
@@ -19,7 +20,8 @@ with open(params.FREQUENT_KMERS, 'rb') as kmercsv:
         kmer, gene_count, genes = row
         gene_count = int(gene_count)
         if gene_count >= 10:
-            kmers.append(Kmer(kmer, int(gene_count), genes))
+            kmers.append(Kmer(kmer, gene_count, genes))
+            gene_count_dict[kmer] = gene_count
 
 # draw kmer graph
 G = nx.Graph()
@@ -29,13 +31,13 @@ for i in xrange(len(G.nodes())):
     for j in xrange(i+1, len(G.nodes())):
         kmerA, kmerB = G.nodes()[i], G.nodes()[j]
         # decide if there should be an edge between kmerA and kmerB
-        if utils.hamdist(kmerA, kmerB) <=5:
+        if utils.hamdist(kmerA, kmerB) <= params.MIN_HAM_DIST:
             G.add_edge(kmerA, kmerB)
-
-print G.nodes()
 
 # plot graph
 import matplotlib.pyplot as plt
-color_values = [k.gene_count for k in kmers] # TODO: fix label coloring, there might be a bug here...
-nx.draw(G, with_labels=True, node_color=color_values, cmap=plt.cm.Reds_r)
-plt.show()
+color_values = [90 - gene_count_dict[kmer] for kmer in G.nodes()]
+plt.figure(figsize=(20,15))
+nx.draw(G, with_labels=True, node_color=color_values, vmin=5, vmax=86, cmap=plt.cm.Reds_r)
+
+plt.savefig('outputs/graph - edges for hamming distance %s.png' % params.MIN_HAM_DIST)
--- params.py
+++ params.py
@@ -1,4 +1,7 @@
+# For kmer counting
 HUMAN_PROTEOME = 'data/Uniprot-Swiss-prot (Homo Sapiens) without fragments.fasta'
 K = 10
 
+# For graph drawing
 FREQUENT_KMERS = 'outputs/frequent k10-mers - 2018-02-20-132439.csv'
+MIN_HAM_DIST = 3
