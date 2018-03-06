8edca17db26872fe0666b3aa8e6698b634891e38
Gal Ron <galron2000@gmail.com>
2018-03-06 11:03:22 +0200

    Dilute similar proteins in connected components

--- main3_analysis_of_connected_components.py
+++ main3_analysis_of_connected_components.py
@@ -1,7 +1,10 @@
+from Bio import SeqIO
 import csv
 import ast
 # local imports
+from progressbar import Progressbar
 import params
+import utils
 
 # Connected components from graph w/ hamming distance <= 3
 cc1 = set(['AAAAAAAAAA', 'PAAAAAAAAA', 'GAAAAAAAAA', 'RAAAAAAAAA', 'AAAAAAAAAG', 'AVAAAAAAAA', 'AAAAAAVAAA', 'SAAAAAAAAA', 'AAAAAAAAAV', 'AAAAAAAAAL', 'AAAAAAAAGG', 'VAAAAAAAAA', 'ASAAAAAAAA', 'AAAAAAAAAT', 'AAAAAAAAAS', 'LAAAAAAAAA', 'AAAAAAAASS', 'AAVAAAAAAA', 'TAAAAAAAAA', 'AAAVAAAAAA'])
@@ -32,11 +35,46 @@ genes_in_connected_component = set()
 for kmer in connected_component:
     genes_in_connected_component.update(kmers[kmer].genes)
 
+# dilute similar proteins
+protein_seq = dict() # protein_seq[GENE_NAME] = AMINO_ACID_SEQUENCE
+created_protein_names = set() # prevent creation of two similar protein objects
+for rec in SeqIO.parse(open(params.HUMAN_PROTEOME), 'fasta'):
+    seq = str(rec.seq)
+    uniqueIdentifier, entryName, proteinName, organismName, geneName = \
+        utils.parse_UniProtKB_header(rec.description)
+    if geneName == '':
+        print 'Ignoring unknown gene: %s' % rec.description
+        continue
+    if geneName in created_protein_names:
+        print 'Ignoring duplicate gene: %s' % rec.description
+        continue
+    # create a new Protein object
+    created_protein_names.add(geneName)
+    protein_seq[geneName] = seq
+
+pb = Progressbar('Diluting similar proteins')
+diluted_genes_in_connected_component = set()
+i = 0
+for gene1 in genes_in_connected_component:
+    i += 1
+    pb.update_progress(i, len(genes_in_connected_component))
+    redundantProt = False
+    for gene2 in diluted_genes_in_connected_component:
+        if not utils.proteins_are_dissimilar(gene1, gene2,
+                                         protein_seq[gene1], protein_seq[gene2]):
+            redundantProt = True
+            break
+    if not redundantProt:
+        diluted_genes_in_connected_component.add(gene1)
+
+print "Total genes in connected component: %s" % len(genes_in_connected_component)
+print "After removing similar proteins: %s" % len(diluted_genes_in_connected_component)
+
 # write results to file
 with open("outputs/connected component 4.txt", 'wb') as file:
-    file.write('In total %d genes in connected component:\n' %
-               len(genes_in_connected_component))
-    for gene in genes_in_connected_component:
+    file.write('In total %d genes in connected component (after dilution):\n' %
+               len(diluted_genes_in_connected_component))
+    for gene in diluted_genes_in_connected_component:
         file.write(gene + '\n')
     file.write('------------------\n')
     for kmer in connected_component:
